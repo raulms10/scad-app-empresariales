381f0cd5b71aecfdb3fed2f250c40b5ea24f9749
Sebastian Montoya J <yatocapo@gmail.com>
2018-04-13 19:27:31 -0500

    Trabajando en el metodo search

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,6 +11,11 @@ import models.Booking
 import models.Home
 import play.api.db._ // Este es especialmente necesario para conectarse con la BD
 
+import org.joda.time.Days
+import com.github.nscala_time.time.Imports._ // Necesario manipular y hacer calculos con las fechas
+import java.text.SimpleDateFormat; // Necesarios para formatear las fechas
+
+
 // Controlador de la pagina web
 // NOTA: No olvidar poner >>> db: Database como parametro de la clase. OJO!
 @Singleton
@@ -77,12 +82,80 @@ class HomeController @Inject()(db: Database, cc: ControllerComponents) extends A
     }
   }
   
-  /*
-  // Metodo para recuperar los inmuebles indicados en el json del cuerpo
-  def search = Action {
+  // Metodo para recuperar los inmuebles indicados segun el json del cuerpo
+  def search = Action {implicit request =>
+    val cuerpoJson = request.body.asJson.get // En primer lugar se recupera el cuerpo del mensaje el cual debe contener el json con los parametros de busqueda y las fechas de hospedaje
+
+    // Luego, separo los parametros en variables independientes para asi operar con mayor facilidad dichos datos
+    val arrivedDate = cuerpoJson("checkIn").toString.filterNot("\"".toSet)
+    val departureDate = cuerpoJson("checkOut").toString.filterNot("\"".toSet)
+    val cityCode = cuerpoJson("city").toString.filterNot("\"".toSet)
+    val homeType = cuerpoJson("type").toString.filterNot("\"".toSet)
+
+    // Ahora, calculo el numero de dias que se quiere hospedar el cliente por lo que ...
+    val dateFormat = new SimpleDateFormat("dd/MM/yyyy") // Especifico el formato de fecha que voy a manejar
+    val firstDate = new DateTime(arrivedDate + " 00:00:00")
+    val secondDate = new DateTime(departureDate + " 00:00:00")
+    val auxDays = Days.daysBetween(firstDate, secondDate);
+    val numDays = auxDays.getDays();
     
+    Ok(" Dias: " +numDays)
+
+
+
+
+
+
+    /*
+    
+    val diferencia = firstDate.getTime - secondDate.getTime
+    val numDays = Math.abs(TimeUnit.DAYS.convert(diferencia, TimeUnit.MILLISECONDS))
+    */
+
+/*
+    Ok("Parametros: " + arrivedDate + 
+        " ! " + departureDate + " ! " 
+        + cityCode + " ! " + homeType
+        + " ~ " + numDays)
+
+*/
+
+
+
+
+
+
+    /*
+    // Primero, se crea una lista vacia para manejar los datos de los inmuebles que lleguen de la BD
+    var arrayHomes = List[Home]()
+
+    // Luego creamos una variable para realizar la conexion con la BD
+    val conexion = db.getConnection()
+
+    try{
+      // Ahora  creamos una variable en donde formularemos nuestra queries SQL de busqueda
+      val query = conexion.createStatement
+
+
+
+
+
+
+
+
+
+
+
+    }
+    catch {
+      // En caso de error, retornamos un mensaje al respecto
+      case _: Throwable => BadRequest(Json.obj("status" -> "Error", "message" -> "Hubo un error!"))
+    }
+    finally {
+      // Antes de terminar (sea que la consulta sea exitosa o no), cerramos la conexion a la BD
+      conexion.close()
+    }*/
   }
-  */
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
--- build.sbt
+++ build.sbt
@@ -19,3 +19,6 @@ libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2
 // Estas son las dependencias necesarias para trabajar con bases de datos MySQL
 libraryDependencies += jdbc
 libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.41"
+
+// Dependencia para trabajar con JodaTime
+libraryDependencies += "com.github.nscala-time" %% "nscala-time" % "2.18.0"
