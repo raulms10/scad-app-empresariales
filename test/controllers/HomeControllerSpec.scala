bb077ea76a06a9bacd7db90edbd97e403ba57b82
Sebastián Montoya Jiménez <yatocapo@gmail.com>
2018-05-31 20:21:29 -0500

    Se terminan las pruebas de la funcion booking
    Como tal ya termine las pruebas de la funcion booking y de otras
    funciones de HomoController. No obstante, algunas pruebas de booking van
    a quedarse comentadas ya que requieren un token valido de Firebase, lo
    cual si bien puede funcionar durante un tiempo, apenas se revoque la
    validez del mismo token entonces esas pruebas fallaran. Por lo tanto, no
    pueden ser pruebas permanentes.

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -11,7 +11,9 @@ import play.api.libs.json._
 // Clase para ejecutar las pruebas del backend YoTeArriendo de scala
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
   
-  /*" ------ Pruebas para las funciones del realase # 1 ------" should {
+  val TESTTOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImFhNzE5ZDE4MjQ2OTAyN2ZkYWQ5YzVlMjVmNTA0NWUzZjRhZTBjMTAifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20veW90ZWFycmllbmRvLWQ1MzJmIiwibmFtZSI6IlNlYmFzdGnDoW4gTW9udG95YSBKaW3DqW5leiIsInBpY3R1cmUiOiJodHRwczovL2xoMy5nb29nbGV1c2VyY29udGVudC5jb20vLTU4eWxud3FJNmhBL0FBQUFBQUFBQUFJL0FBQUFBQUFBQURVL3VsRi15UDJIcmVnL3Bob3RvLmpwZyIsImF1ZCI6InlvdGVhcnJpZW5kby1kNTMyZiIsImF1dGhfdGltZSI6MTUyNzgxNDI4MSwidXNlcl9pZCI6Ik03eHl1UjU3VnlUYU9WT2VlNXVhaktTVk5LQTMiLCJzdWIiOiJNN3h5dVI1N1Z5VGFPVk9lZTV1YWpLU1ZOS0EzIiwiaWF0IjoxNTI3ODE0MjgxLCJleHAiOjE1Mjc4MTc4ODEsImVtYWlsIjoieWF0b2NhcG9AZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZ29vZ2xlLmNvbSI6WyIxMTE3MzgwODkwMjI5NzI3NDQ0MjkiXSwiZW1haWwiOlsieWF0b2NhcG9AZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoiZ29vZ2xlLmNvbSJ9fQ.C_0KbGwZb6u1NjqR8lTiWxEs0rnuxc9TqhNDKXwV_ZfHaKs7CxzQVPKjr0uHMryiQZk3x83xt8MBkfVOYqgDeNaby3Jcqm3e9CD2K1U2KaCHbX5Sl_2_04ApoXQN6TvJsIaphyRRo4c3N662N5MfVksbB1cnezyT3z7Mx7XzKS7_IHGo99LH1zOIViArsNDtw-7RIfgO0Qd-vEhCYSZes8_rS8vrLaEy5IURIvAZq9GnUmg6bJhs4QiY0Y3HYftvrIWtD4Ix6XrhRQ3gUN4v2fiibuOjt6vncuROwxj-USms50ZgaVY_EVclPfz_NhXTF67qp--YKBKQfdLgiQ8TRQ" // Poner aqui un token valido para hacer las pruebas del Realease # 2
+  
+  " ------ Pruebas para las funciones del realase # 1 ------" should {
     
     "Prueba inicial para revisar si el index esta funcionando" in {
       val controller = inject[HomeController]
@@ -205,9 +207,7 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"homes":[{"id":1,"name":"Las Brisas 2","description":"40m2, 2 habitaciones y un baño","location":{"address":"Calle 56a #49-70","latitude":"41.40338","longitude":"2.17403"},"city":"Colombia-Medellin","type":"Apartamento","rating":4.2,"totalAmount":100.25,"pricePerNight":100.25,"thumbnail":"https://es.seaicons.com/wp-content/uploads/2016/09/Actions-go-home-icon.png"},{"id":5,"name":"Turmalina","description":"36m2, 4 habitaciones, 1 baño con jacuzzi, luz natural e Internet","location":{"address":"Circular 3 #71-13","latitude":"6.245713","longitude":"-75.590658"},"city":"Colombia-Medellin","type":"Apartamento","rating":1.6,"totalAmount":168.98,"pricePerNight":168.98,"thumbnail":"https://cdn4.iconfinder.com/data/icons/free-large-business-icons/256/Two-storied_house_SH.png"},{"id":8,"name":"REGATA","description":"45.6m2, 6 habitaciones, 2 baños (1 con jacuzzi), terraza, desayuno gratis e Internet","location":{"address":"Carrera 52 #123-95","latitude":"6.309076","longitude":"-75.555521"},"city":"Colombia-Medellin","type":"Casa","rating":4.8,"totalAmount":305.84,"pricePerNight":305.84,"thumbnail":"https://vignette.wikia.nocookie.net/zombiefarm/images/6/66/Log_Cabin.png/revision/latest?cb=20110114173519"}]}""")
     }
     
-    
   }
-  */
   
   " ------ Pruebas para las funciones del realase # 2 ------" should {
     
@@ -229,12 +229,233 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       r mustBe "ERROR!"
     }
     
+    "Prueba para revisar que la funcion booking - Caso 1: Request vacio" in {
+      val controller = inject[HomeController]
+      val r = controller.bookingFunction(None, None) // Este 2do parametro no afecta en nada la prueba
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Request vacio!!!"}""")
+    }
     
+    "Prueba para revisar que la funcion booking - Caso 2: No estan todos los parametros" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "10-04-2018"
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), None) // Este 2do parametro no afecta en nada la prueba
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Request no tiene todos los parametros indicados"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 3: No se envio token de autenticacion" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "10-04-2018",
+          "id": 5
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), None) // El None en este 2do parametro es como no enviar el token
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"No hay ninguna clave token en el encabezado"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 4: Token no valido" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "10-04-2018",
+          "id": 5
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some("123456"))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Token de usuario invalido"}""")
+    }
+    
+    
+    // ----- Pruebas de booking que requieren un token valido  -----
+    /*
+    "Prueba para revisar que la funcion booking - Caso 5: Fechas con formato incorrecto (Ej: Numeros)" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": 7042018,
+          "checkOut": 10042018,
+          "id": 1
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Las fechas deben ser tipo String"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 6: Id de la casa no es un numero" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "10-04-2018",
+          "id": "5"
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"El ID del inmueble debe ser un numero"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 7: Id de la casa no existe" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "10-04-2018",
+          "id": -5
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"El ID del inmueble no existe en la BD"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 8: Fechas sin formato correcto" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07 04 2018",
+          "checkOut": "10~04~2018",
+          "id": 1
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Las fechas no tienen el formato DD/MM/YYYY o DD-MM-YYYY"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 9: Fechas invertidas" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "4-04-2018",
+          "id": 1
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Las fecha de partida no puede ser anterior a la fecha de llegada!"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 10: Reserva de CERO DIAS" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "07-04-2018",
+          "checkOut": "7-04-2018",
+          "id": 1
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"La reserva debe ser de por lo menos de un dia!"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 11: Fechas solapadas" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "04-05-2019",
+          "checkOut": "21-05-2019",
+          "id": 2
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":0,"mensaje":"Reserva invalida por fechas solapadas! El inmueble esta ocupado del 15-5-2019 al 19-5-2019"}""")
+    }
+    
+    "Prueba para revisar que la funcion booking - Caso 12: Reserva exitosa" in {
+      val controller = inject[HomeController]
+      val jsonAux: JsValue = Json.parse("""
+        {
+          "checkIn": "04-05-2019",
+          "checkOut": "21-05-2019",
+          "id": 9
+        }
+      """)
+      val r = controller.bookingFunction(Some(jsonAux), Some(TESTTOKEN))
+      
+      if (r == None)
+      {
+        r mustBe None
+      }
+      
+      r.get mustBe Json.parse("""{"agency":{"nit":"1234-4567-00048-6553","name":"Arrendamientos SCAD","description":"Agencia de arrendamientos para estudiantes de la Universidad de Antioquia."},"codigo":1,"mensaje":"Reserva con exito!!!"}""")
+    }
+    */
+    // -------------------------------------------------------------
     
     
   }
-  
-  /*
   
   " --------- Pruebas para las funciones de apoyo ----------" should {
     
@@ -329,5 +550,5 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
       r mustBe false
     }
     
-  }*/
+  }
 }
